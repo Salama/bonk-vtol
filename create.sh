d639bf2f0436494b7109a928ac9f6e14a4ac8a19
Salama <20580796+Salama@users.noreply.github.com>
2021-11-07 17:43:52 +0200

    Add builds for extensions and userscripts

--- create.sh
+++ create.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-7z a -tzip vtol.zip manifest.json injector.js loadInjector.js runInjectors.js background.js
+name="Bonk VTOL"
+sname="bonk-vtol"
+version="4.5"
+description="Brings back the legendary gamemode known as VTOL to bonk.io"
+
+tmp0=$(mktemp)
+tmp1=$(mktemp)
+
+if [ ! -d build ]; then mkdir build; fi
+cd build
+csplit ../injector.js /\/\*\*\*REPLACERS\*\*\*/
+cat xx00 > "$tmp0"
+cat ../mod/replacers.js >> "$tmp0"
+cat xx01 | tail -n+2 >> "$tmp0"
+csplit "$tmp0" /\/\*\*\*CONSTANTS\*\*\*/
+cat xx00 > "$tmp0"
+cat ../mod/constants.js >> "$tmp0"
+cat xx01 | tail -n+2 >> "$tmp1"
+
+cat ../manifest.json | jq ".name=\"$name\" | .version=\"$version\" | .description=\"$description\"" > "$tmp0"
+
+7z a -tzip "$sname.zip" "$tmp0" "$tmp1" ../loadInjector.js ../runInjectors.js ../background.js
+
+csplit ../userscript.js /\/\*\*\*REPLACERS\*\*\*/
+cat xx00 > "$tmp0"
+cat ../mod/replacers.js >> "$tmp0"
+cat xx01 | tail -n+2 >> "$tmp0"
+csplit "$tmp0" /\/\*\*\*CONSTANTS\*\*\*/
+cat xx00 > "$tmp0"
+cat ../mod/constants.js >> "$tmp0"
+cat xx01 | tail -n+2 >> "$tmp0"
+cat "$tmp0" | sed "s#/\*\*\*NAME\*\*\*/#$name#g" | sed "s#/\*\*\*SNAME\*\*\*/#$sname#g" | sed "s#/\*\*\*VERSION\*\*\*/#$version#g" | sed "s#/\*\*\*DESCRIPTION\*\*\*/#$description#g" > "$sname.js"
+rm xx00 xx01 "$tmp0" "$tmp1"
---
+++ mod/replacers.js
@@ -0,0 +1,5 @@
+newStr=newStr.replace('"v"){;}',VTOL);
+newStr=newStr.replace('S9L.W1E(116)];',VTOL_MODE);
+newStr=newStr.replace('build(D2i,h2i) {',RENDER_JETPACK);
+newStr=newStr.replace('P1R[43][P1R[7][551]][S9L.W1E(62)]={lobbyName:S9L.C1E(2070),gameStartName:S9L.W1E(2070),lobbyDescription:S9L.W1E(2071),tutorialTitle:S9L.C1E(2072),tutorialText:S9L.C1E(2073),forceTeams:false,forceTeamCount:null,editorCanTarget:false};', MAP_EDITOR_VTOL);
+newStr=newStr.replace('function w8I(){', END_GAME);
