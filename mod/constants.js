ae7fcc30b669a175eff724e5941b4df90881852f
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 18:40:20 +0200

    Update and add particles

--- mod/constants.js
+++ mod/constants.js
@@ -1,85 +1,118 @@
 let END_GAME=`
-function w8I() {
-	if(j0V[94]["hostID"] == j0V[94]["getLSID"]()) {
-		j0V[94]["sendReturnToLobby"]();
+function i5S() {
+	if(u6H[11]["hostID"] == u6H[11]["getLSID"]()) {
+		u6H[11]["sendReturnToLobby"]();
 	}
 `;
 
 let RENDER_JETPACK=`
-build(D2i,h2i) {
-	if(this["gameSettings"]["mo"]=="v") {
-		this["VTOLWing"]=new PIXI["Graphics"]();
-		this["VTOLWing"]["beginFill"](0xcccccc);
-		this["VTOLWing"]["drawRect"](
-			this["radius"]*(-V["footHW"]+V["footOffsetX"]),
-			this["radius"]*(-V["footHH"]+V["footOffsetY"]),
-			this["radius"]*(V["footHW"]*2),
-			this["radius"]*(V["footHH"]*2)
+if(this["gameSettings"]["mo"]=="v") {
+	this["VTOLWing"]=new PIXI["Graphics"]();
+	this["VTOLWing"]["beginFill"](0xcccccc);
+	this["VTOLWing"]["drawRect"](
+		this["radius"]*(-{uniVar}["footHW"]+{uniVar}["footOffsetX"]),
+		this["radius"]*(-{uniVar}["footHH"]+{uniVar}["footOffsetY"]),
+		this["radius"]*({uniVar}["footHW"]*2),
+		this["radius"]*({uniVar}["footHH"]*2)
+	);
+	this["VTOLWing"]["beginFill"](0xcccccc);
+	this["VTOLWing"]["drawRect"](
+		this["radius"]*(-{uniVar}["footHW"]+-{uniVar}["footOffsetX"]),
+		this["radius"]*(-{uniVar}["footHH"]+{uniVar}["footOffsetY"]),
+		this["radius"]*({uniVar}["footHW"]*2),
+		this["radius"]*({uniVar}["footHH"]*2)
 		);
-		this["VTOLWing"]["beginFill"](0xcccccc);
-		this["VTOLWing"]["drawRect"](
-			this["radius"]*(-V["footHW"]+-V["footOffsetX"]),
-			this["radius"]*(-V["footHH"]+V["footOffsetY"]),
-			this["radius"]*(V["footHW"]*2),
-			this["radius"]*(V["footHH"]*2)
-			);
-		this["container"]["addChild"](this["VTOLWing"]);
-	}`;
-
-let VTOL_MODE=`"v",S9L.W1E(116)];` //Adds VTOL to mode selection button
+	this["container"]["addChild"](this["VTOLWing"]);
+}`;
 
 let VTOL=`"v") {
-	var O1B=[];
-	O1B[11]=(-22/30)*O7R[445];
-	O1B[791]=0.12;
-	O1B[874]=new P1R[2](0,O1B[11]);
-	O1B[874]=O7R[5][O7R[956]]["body"]["GetWorldVector"](O1B[874],O1B[874]);
-	O1B[857]=new P1R[2](0,O1B[11]*O1B[791]);
-	O1B[857]=O7R[5][O7R[956]]["body"]["GetWorldVector"](
-		O1B[857],
-		O1B[857]
+	var VTOL_O1B=[];
+	VTOL_O1B[11]=(-22/30)*{sizeMultiplier};
+	VTOL_O1B[791]=0.12;
+	VTOL_O1B[874]=new {box2d}.Common.Math.b2Vec2(0,VTOL_O1B[11]);
+	VTOL_O1B[874]={playerObject}["body"]["GetWorldVector"](VTOL_O1B[874],VTOL_O1B[874]);
+	VTOL_O1B[857]=new {box2d}.Common.Math.b2Vec2(0,VTOL_O1B[11]*VTOL_O1B[791]);
+	VTOL_O1B[857]={playerObject}["body"]["GetWorldVector"](
+		VTOL_O1B[857],
+		VTOL_O1B[857]
 	);
-	O1B[347]=O7R[5][O7R[956]]["body"]["GetWorldPoint"](new P1R[2](
-		V["footOffsetX"]*O7R[445],
-		V["footOffsetY"]*O7R[445]
+	VTOL_O1B[347]={playerObject}["body"]["GetWorldPoint"](new {box2d}.Common.Math.b2Vec2(
+		{uniVar}["footOffsetX"]*{sizeMultiplier},
+		{uniVar}["footOffsetY"]*{sizeMultiplier}
 	));
-	O1B[848]=O7R[5][O7R[956]]["body"]["GetWorldPoint"](new P1R[2](
-		-V["footOffsetX"]*O7R[445],
-		V["footOffsetY"]*O7R[445]
+	VTOL_O1B[848]={playerObject}["body"]["GetWorldPoint"](new {box2d}.Common.Math.b2Vec2(
+		-{uniVar}["footOffsetX"]*{sizeMultiplier},
+		{uniVar}["footOffsetY"]*{sizeMultiplier}
 	));
-	O1B[195]="none";
-	if(O7R[0][1][O7R[956]]["up"]) {
-		if(O7R[0][1][O7R[956]]["left"]) {
-			O1B[195]="right";
+	VTOL_O1B[195]="none";
+	if({inputs}["up"]) {
+		if({inputs}["left"]) {
+			VTOL_O1B[195]="right";
 		}
-		else if(O7R[0][1][O7R[956]]["right"]) {
-			O1B[195]="left";
+		else if({inputs}["right"]) {
+			VTOL_O1B[195]="left";
 		}
 		else {
-			O1B[195]="both";
+			VTOL_O1B[195]="both";
 		}
 	}
-	else if(O7R[0][1][O7R[956]]["left"] && O7R[0][1][O7R[956]]["right"]) {
-		O1B[195]="both";
+	else if({inputs}["left"] && {inputs}["right"]) {
+		VTOL_O1B[195]="both";
 	}
-	else if(O7R[0][1][O7R[956]]["left"]) {
-		O1B[195]="right";
+	else if({inputs}["left"]) {
+		VTOL_O1B[195]="right";
 	}
-	else if(O7R[0][1][O7R[956]]["right"]) {
-		O1B[195]="left";
+	else if({inputs}["right"]) {
+		VTOL_O1B[195]="left";
 	}
-	if(O1B[195]=="both") {
-		O7R[5][O7R[956]]["body"]["ApplyImpulse"](O1B[874],O1B[347]);
-		O7R[5][O7R[956]]["body"]["ApplyImpulse"](O1B[874],O1B[848]);
+	if(VTOL_O1B[195]=="both") {
+		{playerObject}["body"]["ApplyImpulse"](VTOL_O1B[874],VTOL_O1B[347]);
+		{playerObject}["body"]["ApplyImpulse"](VTOL_O1B[874],VTOL_O1B[848]);
 	}
-	if(O1B[195]=="left") {
-		O7R[5][O7R[956]]["body"]["ApplyImpulse"](O1B[874],O1B[347]);
-		O7R[5][O7R[956]]["body"]["ApplyImpulse"](O1B[857],O1B[848]);
+	if(VTOL_O1B[195]=="left") {
+		{playerObject}["body"]["ApplyImpulse"](VTOL_O1B[874],VTOL_O1B[347]);
+		{playerObject}["body"]["ApplyImpulse"](VTOL_O1B[857],VTOL_O1B[848]);
 	}
-	if(O1B[195]=="right") {
-		O7R[5][O7R[956]]["body"]["ApplyImpulse"](O1B[857],O1B[347]);
-		O7R[5][O7R[956]]["body"]["ApplyImpulse"](O1B[874],O1B[848]);
+	if(VTOL_O1B[195]=="right") {
+		{playerObject}["body"]["ApplyImpulse"](VTOL_O1B[857],VTOL_O1B[347]);
+		{playerObject}["body"]["ApplyImpulse"](VTOL_O1B[874],VTOL_O1B[848]);
 	}
 }`;
 
-let MAP_EDITOR_VTOL=`P1R[43][P1R[7][551]][S9L.W1E(62)]={lobbyName:S9L.C1E(2070),gameStartName:S9L.W1E(2070),lobbyDescription:S9L.W1E(2071),tutorialTitle:S9L.C1E(2072),tutorialText:S9L.C1E(2073),forceTeams:false,forceTeamCount:null,editorCanTarget:true};`;
+/**YOINKED WITH PERMISSION FROM BLU <3**/
+let VARTOL_PARTICLES = `if (arguments[3].mo == "v") {
+	for (let currDisc = 0; currDisc < arguments[1].discs.length; currDisc++) {
+		if (arguments[0].discs[currDisc] && arguments[1].discs[currDisc] && this.discGraphics[currDisc] && arguments[4] && arguments[4][currDisc]) {
+			let particleSize = 2.5 * this.discGraphics[currDisc].radius / arguments[1].physics.ppm;
+			let percentAlongJetpack = 0.85;
+			let xOffset = -this.discGraphics[currDisc].radius * (E.footOffsetX + -E.footHW) * percentAlongJetpack;
+	
+			let spreadWidth = .3;
+			let spreadDir = Math.random() * spreadWidth - spreadWidth / 2;
+			let dir = arguments[1].discs[currDisc].a + spreadDir + Math.PI / 2;
+			let avgSpeed = 2;
+			let maxRandSpeed = .7;
+			let speed = avgSpeed + (Math.random() * maxRandSpeed) - (maxRandSpeed / 2);
+			let particleXV = Math.cos(dir) * speed;
+			let particleYV = Math.sin(dir) * speed;
+			particleXV += arguments[1].discs[currDisc].xv / 30;
+			particleYV += arguments[1].discs[currDisc].yv / 30;
+	
+			let fireJetpack = [];
+			if(arguments[4][currDisc].left && !arguments[4][currDisc].action2) fireJetpack.push("right");
+			if(arguments[4][currDisc].right && !arguments[4][currDisc].action2) fireJetpack.push("left");
+			if(arguments[4][currDisc].up && !fireJetpack.length) fireJetpack = ["left", "right"];
+			for(let jetpack in fireJetpack) {
+				let particle = new PIXI.Graphics;
+				// vanilla vtol (with old renderer lighting fx accounted for) = 0xffffd9;
+				particle.beginFill(0xffd9d9);
+				particle.drawRect(0, -particleSize/2, particleSize, particleSize);
+				particle.x = this.discGraphics[currDisc].container.x + ((fireJetpack[jetpack] == "right" ? xOffset : -xOffset) * Math.cos(this.discGraphics[currDisc].container.rotation));
+				particle.y = this.discGraphics[currDisc].container.y + ((fireJetpack[jetpack] == "right" ? xOffset : -xOffset) * Math.sin(this.discGraphics[currDisc].container.rotation));
+				this.blurContainer.addChild(particle);
+				this.particleManager.container.addChild(particle);
+				this.particleManager.particles.push({graphics: particle, xv: particleXV, yv: particleYV, alpha: 1, shrinkPerFrame: 0.05, gravity: .04});
+			}
+	  	}
+	}
+}`;
--- mod/replacers.js
+++ mod/replacers.js
@@ -1,5 +1,29 @@
-newStr=newStr.replace('"v"){;}',VTOL);
-newStr=newStr.replace('S9L.W1E(116)];',VTOL_MODE);
-newStr=newStr.replace('build(D2i,h2i) {',RENDER_JETPACK);
-newStr=newStr.replace('P1R[43][P1R[7][551]][S9L.W1E(62)]={lobbyName:S9L.C1E(2070),gameStartName:S9L.W1E(2070),lobbyDescription:S9L.W1E(2071),tutorialTitle:S9L.C1E(2072),tutorialText:S9L.C1E(2073),forceTeams:false,forceTeamCount:null,editorCanTarget:false};', MAP_EDITOR_VTOL);
-newStr=newStr.replace('function w8I(){', END_GAME);
+let uniVar=newStr.match(/\(0,20\)\);[A-Z]/)[0][8];
+let box2d=newStr.match(/requirejs(.){0,70}function\(...,...,.../)[0].slice(-3);
+
+let buildFunction = newStr.match(/build\(...\,...\) \{var ...=\[arguments\];...\[.\]=....;this\[(.){0,30}new PIXI/)[0].split("var ");
+newStr=newStr.replace(buildFunction[0], buildFunction[0] + RENDER_JETPACK.replaceAll('{uniVar}', uniVar));
+
+let sizeMultiplier=newStr.match(/20;\}...\[(\d){1,3}\]\*=...\[(\d){1,3}\]/)[0].split("=")[1];
+let playerObject=newStr.match(/...\[(\d){1,3}\]\[...\[(\d){1,3}\]\]=\{a1a:/)[0].split("=")[0];
+let inputs = `${playerObject.substr(0, 3)}[0][1][${playerObject.match(/\[...\[(\d){1,3}\]\]/)[0].slice(1, -1)}]`;
+newStr=newStr.replace('"v"){;}',
+	VTOL.replaceAll('{box2d}', box2d)
+	.replaceAll('{sizeMultiplier}', sizeMultiplier)
+	.replaceAll('{playerObject}', playerObject)   
+	.replaceAll('{inputs}', inputs)
+	.replaceAll('{uniVar}', uniVar) 
+);
+
+let lobbyModes=newStr.match(/...\....\((\d){1,4}\)\];...\[(\d){1,3}\](.){0,20}\[\];/)[0];
+newStr=newStr.replace(lobbyModes,"'v'," + lobbyModes);
+
+let lastMode = newStr.match(/editorCanTarget:[a-z]{4,5}\};/g).slice(-1);
+let modeVar = newStr.match(/editorCanTarget:[a-z]{4,5}\};...\[(\d){1,3}\]\[...\[(\d){1,3}\]\[(\d){1,3}\]\]/)[0].split(";")[1];
+newStr=newStr.replace(lastMode, lastMode + modeVar + "['v'].editorCanTarget=true;");
+
+//let gameEndFunction = newStr.match(`function ...\(\)\{var ...=\[arguments\];(.){0,50}if\(${varInGameEndFunction.substr(0, 3)}\[(\d){1,3}\]\)\{${varInGameEndFunction.substr(0, 3)}\[(\d){1,3}\]\[...\[(\d){1,3}\]\[(\d){1,3}\]\]\(\);${varInGameEndFunction.substr(0, 3)}\[(\d){1,3}\]\[...\[(\d){1,3}\]\[(\d){1,3}\]\]\(\);`)[0];
+newStr=newStr.replace('function i5S(){', END_GAME);
+
+// add particles to renderer
+newStr = newStr.replace(`this.particleManager.render`, `${VARTOL_PARTICLES} this.particleManager.render`);
